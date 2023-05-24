ae2b30ed59537bf7fd3d2edd0c3d881d0ad30129
Raikonif <raikonif@gmail.com>
2023-05-24 15:01:17 -0400

    day1 project done

--- day1/project.py
+++ day1/project.py
@@ -6,3 +6,19 @@
 # Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 # Write your code below this line 👇
+
+def valditation_input(bill):
+    if float(bill) > 0 and bill.isdigit():
+        return True
+    else:
+        return False
+def tip_calculator(bill, split):
+    is_valid_bill = valditation_input(bill)
+    is_valid_split = valditation_input(split)
+    if is_valid_bill and is_valid_split:
+        bill_formatted = float(bill)
+        tip = (bill_formatted/5) * 1.12
+        frmt_tip = "{:.2f}".format(tip)
+        print(f"Tip: ${frmt_tip}")
+
+tip_calculator(input("Enter the bill: $\n"), input("Enter how many splits do you want: \n"))
