5f019f8b3add6bacecaf3403f620f62f9ac7d5c6
RaquelAlarInt <raquelalarint@gmail.com>
2023-05-25 09:38:57 -0400

    day1

--- day1/exercise4.py
+++ day1/exercise4.py
@@ -21,3 +21,4 @@
 # Hint
 # 1. There are 365 days in a year, 52 weeks in a year and 12 months in a year.
 # 2. Try copying the example output into your code and replacing the relevant parts so that the sentence is formated the same way.\
+rint("me resulto muy complicado deiguito :( ")
