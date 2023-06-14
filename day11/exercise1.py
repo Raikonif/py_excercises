35a97a697ab8bf945446588225348dcd72d5dbfa
Diego Alarcón Inturias <raikonif@gmail.com>
2023-06-14 08:23:39 -0400

    day11 exercises done (#32)

---
+++ day11/exercise1.py
@@ -0,0 +1,11 @@
+# Count Vowels and Consonantsring.
+# Instructions
+# Use global variables to keep track of the counts.
+# Then, ask the user to enter a string, call the function with the input string, and print the counts.
+# Write a function called "count_vowels_consonants" that takes a string as input and counts the number of vowels and consonants
+# in the string. Use global variables to keep track of the counts. Then, ask the user to enter a string,
+# call the function with the input string, and print the counts.
+
+# Hint:
+# 1. Learn About Global and Local Variables
+# 2. Use Functions for best practices
