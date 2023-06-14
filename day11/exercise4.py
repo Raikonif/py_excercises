3d71cda10558424e52ea3eab7f999d7ed19d1988
Raikonif <raikonif@gmail.com>
2023-06-14 08:22:32 -0400

    day11 exercises done

---
+++ day11/exercise4.py
@@ -0,0 +1,6 @@
+# Access Global Variable
+# Instructions
+# Write a function called "access_global_variable" that does not take any input.
+# Access a global variable inside the function and print its value.
+# Then, assign a new value to the global variable. Finally, ask the user to enter a number, call the function,
+# and print the updated global variable.
