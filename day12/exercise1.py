cdc6810037b21ba4fdf8ef4acb7b0a2214433816
Raikonif <raikonif@gmail.com>
2023-06-15 11:37:49 -0400

    day 12 excercises added

---
+++ day12/exercise1.py
@@ -0,0 +1,4 @@
+# Countdown Timer
+# Instructions
+# Write a program that asks the user to enter a number and then counts down from
+# that number to 0 using a while loop. Use a global variable to keep track of the countdown.
---
+++ day12/exercise4.py
@@ -0,0 +1,4 @@
+# Number Guessing Game
+# Instructions
+# Write a program that generates a random number between 1 and 100 and asks the user to guess the number.
+# Provide feedback if the guess is too high or too low. Use a while loop and a global variable to keep track of the number of guesses.
