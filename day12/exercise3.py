9e9016336ae17515bee47cae0ef27ef7a63d7f53
Diego Alarcón Inturias <raikonif@gmail.com>
2023-06-15 11:38:50 -0400

    day 12 excercises added (#34)

---
+++ day12/exercise3.py
@@ -0,0 +1,4 @@
+# Factorial Calculator
+# Instructions
+# Write a program that asks the user to enter a positive integer and then calculates its factorial
+# using a while loop. Use a global variable to keep track of the current multiplication factor.
