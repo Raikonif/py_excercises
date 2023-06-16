709ac342cba571a492e86a2ae1163a39d5116235
Diego Alarcón Inturias <raikonif@gmail.com>
2023-06-16 07:38:46 -0400

    day 13 exercises added (#36)

---
+++ day13/exercise3.py
@@ -0,0 +1,6 @@
+# Reverse String
+# Write a program that takes a string as input and reverses it, then prints the reversed string.
+
+# Hints:
+# Define a function called reverse_string(input_string) that takes a string as input and returns the reversed string.
+# Use string slicing to reverse the string.
