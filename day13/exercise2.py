9fb49a483ea4b2cb1e5a56bd22456720a1665cb7
RaquelAlarInt <raquelalarint@gmail.com>
2023-06-16 16:27:14 -0400

    DAY13

--- day13/exercise2.py
+++ day13/exercise2.py
@@ -6,3 +6,13 @@
 # Define a function called get_unique_elements(numbers) that takes a list of numbers as input
 # and returns a new list containing only the unique elements.
 # Use a for loop to iterate over the numbers and check if each element is already present in the new list before adding it.
+def get_unique_elements(numbers):
+    return set(numbers)
+    numbers = numbers.repleace(" ", "")
+    
+
+
+numbers = input("Ingrese una lista de numeros para destacar entre ellos:\n")
+resultado = get_unique_elements(numbers)
+print(numbers)
+print(resultado)
--- day13/exercise3.py
+++ day13/exercise3.py
@@ -4,3 +4,7 @@
 # Hints:
 # Define a function called reverse_string(input_string) that takes a string as input and returns the reversed string.
 # Use string slicing to reverse the string.
+print("Bienvenido para invertir palabra!")
+input_string = input("Ingrese palabra:\n")
+new_string = input_string[::-1]
+print(new_string)
