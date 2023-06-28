3933c7e7935922d04ae411a4eddb34df516d73a7
RaquelAlarInt <raquelalarint@gmail.com>
2023-06-28 16:33:26 -0400

    DAY14

--- day14/project_guessing_game.py
+++ day14/project_guessing_game.py
@@ -7,3 +7,43 @@
 # Track the number of turns remaining.
 # If they run out of turns, provide feedback to the player.
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+intentos = 0
+number = 50
+
+print("Bienvenido al juego Adivina Adivinador")
+name = input("Ingrese nombre jugador:\n")
+modo = input("Ingrese modo de Juego: 1=Easy 2=Hard:\n")
+print("Empecemos", name, ".Estoy pensando en un numero del 1 al 100")
+if modo == "1":
+    while intentos <5:
+        print("Adivina")
+        player = int(input("El numero:\n"))
+
+        intentos = intentos +1
+
+        if player < number:
+            print("Numero muy bajo")
+        elif player > number:
+            print("Numero muy alto")
+        elif player == number:
+            break
+    if player == number:
+        intentos = str(intentos)
+        print("GANASTE!!!", name, "Adivinaste en:", intentos, "intentos")
+elif modo == "2":
+    while intentos <3:
+        print("Adivina")
+        player = int(input("El numero:\n"))
+
+        intentos = intentos +1
+
+        if player < number:
+            print("Numero muy bajo")
+        elif player > number:
+            print("Numero muy alto")
+        elif player == number:
+            break
+    if player == number:
+        intentos = str(intentos)
+        print("GANASTE!!!", name, "Adivinaste en:", intentos, "intentos")
+
