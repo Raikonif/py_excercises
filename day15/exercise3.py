5542a4c0e25edc04ae260ffb440ccc7ef0e4a112
raikonif <raikonif@gmail.com>
2023-06-30 11:56:20 -0400

    day 15 exercises added

---
+++ day15/exercise3.py
@@ -0,0 +1,3 @@
+# Exercise 3: Dog Class
+# Create a class called Dog to represent a basic dog. 
+# It should have attributes like name and age. Add methods to make the dog bark and display its information.
