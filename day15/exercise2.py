92f3b33979aa76c09c7dfbc7196286d6610f2cff
raikonif <raikonif@gmail.com>
2023-06-30 11:57:58 -0400

    day 15 exercises formatted

--- day15/exercise2.py
+++ day15/exercise2.py
@@ -1,3 +1,3 @@
 # Exercise 2: Bank Account Class
-# Create a class called BankAccount to represent a basic bank account. 
+# Create a class called BankAccount to represent a basic bank account.
 # It should have attributes like account_number and balance. Add methods to deposit, withdraw, and display the account balance.
--- day15/exercise3.py
+++ day15/exercise3.py
@@ -1,3 +1,3 @@
 # Exercise 3: Dog Class
-# Create a class called Dog to represent a basic dog. 
+# Create a class called Dog to represent a basic dog.
 # It should have attributes like name and age. Add methods to make the dog bark and display its information.
