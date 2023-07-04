af20be2c610fbda2f79717d81751c3fd892fa0da
Raikonif <raikonif@gmail.com>
2023-07-04 09:04:24 -0400

    day 17 exercises added

---
+++ day17/exercise3.py
@@ -0,0 +1,6 @@
+# Exercise 3: Online Shopping System
+# Create classes to represent a simple online shopping system.
+# You should have classes for Customer, Product, Cart, and Order.
+# The Cart class should allow customers to add and remove products before placing an order.
+
+# Hint: Consider using aggregation to represent the relationships between classes (e.g., a Customer has a Cart).
---
+++ day17/exercise4.py
@@ -0,0 +1,5 @@
+# Exercise 4: Social Media Application
+# Create classes to represent a social media application. You should have classes for User, Post, and Comment.
+# Users can create posts and comment on each other's posts.
+
+# Hint: Use composition to represent the relationships between classes (e.g., a Post has a User and multiple Comments).
