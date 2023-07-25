9859e1cc4a697d5bd45b0075b954686f3b373270
Diego Alarcón Inturias <raikonif@gmail.com>
2023-07-25 10:43:07 -0400

    day19 exercises and guide added (#48)

---
+++ day19/exercise1.py
@@ -0,0 +1,3 @@
+# Exercise 1: Employee Management System
+# Create classes to represent an employee management system. You should have classes for Employee,
+# Department, Project, and Company. Employees can be assigned to departments and projects.
---
+++ day19/exercise2.py
@@ -0,0 +1,4 @@
+# Exercise 2: Online Music Streaming Platform
+# Create classes to represent an online music streaming platform.
+# You should have classes for User, Song, Album, Playlist, and Library.
+# Users can create playlists, add songs to their library, and listen to music.
